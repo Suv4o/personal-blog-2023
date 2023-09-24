d4e521e06cfde1ce194dfac660b1cae2ca348bc9
Aleks <aleksandar.trpkovski@gmail.com>
2023-09-24 16:50:51 +1000

    update color to colours spelling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,28 +4,28 @@ export default {
     theme: {
         extend: {
             colors: {
-                primary: "rgb(var(--color-primary) / <alpha-value>)", // #ee5f53
-                "primary-light": "rgb(var(--color-primary-light) / <alpha-value>)", // #f1918b
-                secondary: "rgb(var(--color-secondary) / <alpha-value>)", // #173353
-                "secondary-light": "rgb(var(--color-secondary-light) / <alpha-value>)", // #37506e
-                "green-light": "rgb(var(--color-green-light) / <alpha-value>)", // #00dd82
-                green: "rgb(var(--color-green) / <alpha-value>)", // #40979d
-                beige: "rgb(var(--color-beige) / <alpha-value>)", // #d8d7c1
-                "beige-light": "rgb(var(--color-beige-light) / <alpha-value>)", // #efe8df
-                "beige-gray": "rgb(var(--color-beige-gray) / <alpha-value>)", // #9b9b9b
-                gray: "rgb(var(--color-gray) / <alpha-value>)", // #6d6e71
-                "linkedin-blue": "rgb(var(--color-linkedin-blue) / <alpha-value>)", // #1786b1
-                javascript: "rgb(var(--color-javascript) / <alpha-value>)", // #ffd700
-                firebase: "rgb(var(--color-firebase) / <alpha-value>)", // #ff9900
-                node: "rgb(var(--color-node) / <alpha-value>)", // #3e863d
-                backend: "rgb(var(--color-backend) / <alpha-value>)", // #4d1300
-                css: "rgb(var(--color-css) / <alpha-value>)", // #a30d8a
-                typescript: "rgb(var(--color-typescript) / <alpha-value>)", // #3075bf
-                nestjs: "rgb(var(--color-nestjs) / <alpha-value>)", // #d42148
-                nuxtjs: "rgb(var(--color-nuxtjs) / <alpha-value>)", // #022832
-                vscode: "rgb(var(--color-vscode) / <alpha-value>)", // #3aa8f0
-                vite: "rgb(var(--color-vite) / <alpha-value>)", // #bd34fe
-                other: "rgb(var(--color-other) / <alpha-value>)", // #ee5f53
+                primary: "rgb(var(--colour-primary) / <alpha-value>)", // #ee5f53
+                "primary-light": "rgb(var(--colour-primary-light) / <alpha-value>)", // #f1918b
+                secondary: "rgb(var(--colour-secondary) / <alpha-value>)", // #173353
+                "secondary-light": "rgb(var(--colour-secondary-light) / <alpha-value>)", // #37506e
+                "green-light": "rgb(var(--colour-green-light) / <alpha-value>)", // #00dd82
+                green: "rgb(var(--colour-green) / <alpha-value>)", // #40979d
+                beige: "rgb(var(--colour-beige) / <alpha-value>)", // #d8d7c1
+                "beige-light": "rgb(var(--colour-beige-light) / <alpha-value>)", // #efe8df
+                "beige-gray": "rgb(var(--colour-beige-gray) / <alpha-value>)", // #9b9b9b
+                gray: "rgb(var(--colour-gray) / <alpha-value>)", // #6d6e71
+                "linkedin-blue": "rgb(var(--colour-linkedin-blue) / <alpha-value>)", // #1786b1
+                javascript: "rgb(var(--colour-javascript) / <alpha-value>)", // #ffd700
+                firebase: "rgb(var(--colour-firebase) / <alpha-value>)", // #ff9900
+                node: "rgb(var(--colour-node) / <alpha-value>)", // #3e863d
+                backend: "rgb(var(--colour-backend) / <alpha-value>)", // #4d1300
+                css: "rgb(var(--colour-css) / <alpha-value>)", // #a30d8a
+                typescript: "rgb(var(--colour-typescript) / <alpha-value>)", // #3075bf
+                nestjs: "rgb(var(--colour-nestjs) / <alpha-value>)", // #d42148
+                nuxtjs: "rgb(var(--colour-nuxtjs) / <alpha-value>)", // #022832
+                vscode: "rgb(var(--colour-vscode) / <alpha-value>)", // #3aa8f0
+                vite: "rgb(var(--colour-vite) / <alpha-value>)", // #bd34fe
+                other: "rgb(var(--colour-other) / <alpha-value>)", // #ee5f53
             },
             fontFamily: {
                 sans: ["Ubuntu", "sans-serif"],
