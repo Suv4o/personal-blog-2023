f999f5decdd42147c2f2c34734748e51545e2274
Aleks <aleksandar.trpkovski@gmail.com>
2023-10-15 10:33:28 +1100

    fix(index.ts): initialize Firebase app and Firestore only once to improve performance and prevent unnecessary reinitialization feat(index.ts): add logic to handle existing subscribers and prevent duplicate subscriptions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,11 +10,21 @@
 
 import { onRequest } from "firebase-functions/v2/https";
 import { Validator } from "node-input-validator";
-
+import { initializeApp } from "firebase-admin";
+import { getFirestore } from "firebase-admin/firestore";
 // import * as logger from "firebase-functions/logger";
 // logger.info("Hello logs!", { structuredData: true });
 
+let initialised = false;
+
 export const subscribe_to_newsletters = onRequest(async (request, response) => {
+    if (!initialised) {
+        initializeApp();
+        initialised = true;
+    }
+
+    const db = getFirestore();
+
     const { name, email } = request.body;
 
     const v = new Validator(
@@ -31,10 +41,38 @@ export const subscribe_to_newsletters = onRequest(async (request, response) => {
         }
 
         response.status(400).send(v.errors);
+        return;
     }
 
+    const subscribersRef = db.collection("subscribers");
+    const subscribers = await subscribersRef.where("email", "==", email).get();
+    const [hasBeenSubscribed] = subscribers?.docs?.map((doc) => doc?.data()?.subscribed);
+
+    // Has been subscribed
+    if (subscribers.size > 0 && hasBeenSubscribed) {
+        response.status(400).send({
+            email: {
+                message: "You have already subscribed. Thank you",
+            },
+        });
+        return;
+    }
+
+    // Has not been subscribed. Most likely existing subscriber but unsubscribed
+    if (subscribers.size > 0 && !hasBeenSubscribed) {
+        const [subscriber] = subscribers.docs;
+        await subscriber.ref.update({ subscribed: true });
+        response.send({
+            success: true,
+            message: "Thank you for subscribing again.",
+        });
+        return;
+    }
+
+    await subscribersRef.add({ name, email, subscribed: true });
+
     response.send({
-        name,
-        email,
+        success: true,
+        message: "Thank you for subscribing.",
     });
 });
